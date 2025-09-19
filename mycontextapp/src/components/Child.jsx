2b4da72679a80e5459b7ac1f991fcf521516923d
SRT270 <166096724+SRT270@users.noreply.github.com>
2025-09-19 10:08:05 +0530

    save

---
+++ mycontextapp/src/components/Child.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import {Grandchild}
+const Child = ({gift}) => {
+    return (
+        <h2>I am a Child and i get {gift} from my parent and sending to my child. </h2>
+    )
+}
